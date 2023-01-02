0c8196a12a35287da27b609b1a60a181ffe76f62
smit153 <smitsojitra332003@gmail.com>
2023-01-02 20:55:52 +0530

    deploy

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+  });
+});
+
+app.get("/map", (req, res) => {
+  res.render("map", {
+    title: "About Me",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Page not found.",
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
